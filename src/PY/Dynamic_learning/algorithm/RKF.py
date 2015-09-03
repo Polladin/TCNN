d0e4941b1649a6e03cccb1bcd9cb36170e688682
Polladin <polladin@rambler.ru>
2015-09-03 20:18:09 +0300

    Dynamic learning is done

--- src/PY/Dynamic_learning/algorithm/RKF.py
+++ src/PY/Dynamic_learning/algorithm/RKF.py
@@ -1,22 +1,35 @@
 
 import numpy as np
+import math
 
-step_length = 0.01
+tau = 0.1 #0.05
+k   = 10 #10.0
+D   = 170 #200
+b   = 2.6 #2.6 #1.6 #2.6
+k_in= 1 #1
+T = 0.01
 
-tau = 0.1
-k   = 10.0
-D   = 200
-b   = 2.6
-
-step_length = 0.01
 
 res = np.array([])
 
 def resize_res(NUM_NEURONS):
     np.resize(res, (NUM_NEURONS, 4))
 
+def func_to_calc_sigmoid(u):
+    #print("exp func : " +  str(u))
+    if u < -5:
+        u = -1
+    if u > 5:
+        u = 5
+    #math.exp(-u / step_length)
 
-def solve_one_step(X, S, I, Dxy):
+#     print(2 / (1 + math.exp(-u / step_length)) - 1)
+    return (2 / (1 + math.exp(-u / T)) - 1)
+
+#     return math.copysign(1, u)
+
+
+def solve_one_step(X, S, I, Dxy, step_length):
     tmp_X = X[:]
 
 #     print("solve_one with S:" + str(S) + " I:" + str(I) + " Dxy:" + str(Dxy))
@@ -28,6 +41,8 @@ def solve_one_step(X, S, I, Dxy):
     K4 = calcFunc(calc_K_by_h(tmp_X,K1,step_length), S, I, Dxy);
 
     tmp_X = calc_new_X(X, K1, K2, K3, K4, step_length);
+
+#     tmp_X[2] = func_to_calc_sigmoid(S)
 
     return tmp_X
 
@@ -50,7 +65,7 @@ def calcFunc(X, S, I, Dxy):
     tmp_X = X[:]
     tmp_X[0] = 1
     tmp_X[1] = (-k * X[1] + k * X[2]) / tau
-    tmp_X[2] = (-X[2] + X[3] * X[1] + S + I) / tau
+    tmp_X[2] = (-X[2] + X[3] * X[1] + S + k_in*I) / tau #func_to_calc_sigmoid(X[2]*S) #
     tmp_X[3] = (-b * X[3] - Dxy + b * D) / tau
 
     return tmp_X
--- src/PY/Dynamic_learning/algorithm/chaotic_dyn_learning_NN.py
+++ src/PY/Dynamic_learning/algorithm/chaotic_dyn_learning_NN.py
@@ -3,31 +3,32 @@
 #from Dynamic_learning.algorithm.RKF import *
 
 import Dynamic_learning.algorithm.RKF as rkf
-import numpy as np
-import string
 
 import math
-from _elementtree import tostring
 
-step_length = 0.01
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
-NUM_NEURONS = 25
+step_length = 0.001
+
+
+NUM_NEURONS = 100 #25
 NUM_PATTERNS = 4
 
-LINE_AMOUNT = 5
+LINE_AMOUNT = 10 #5
 
-NUM_STEPS = 100
+NUM_STEPS = 5000
 
-intern_state = [[[0]*4]*NUM_NEURONS]*NUM_STEPS #np.array([])
+intern_state = [[[0]*4 for i in range(NUM_NEURONS)] for j in range(NUM_STEPS)] #np.array([])
 I = [] #[0] * NUM_NEURONS
-W = [[0]*NUM_NEURONS]*NUM_NEURONS #np.array([])
+W = [[0]*NUM_NEURONS for i in range(NUM_NEURONS)] #np.array([])
 
 patterns = [] #np.array([])
 
 
 def read_in_pattern():
     global I
-    pattern_in_file = open("patterns.in", "r")
+    pattern_in_file = open("input_pattern.in", "r")
 
     arr = []
     arr.append([])
@@ -41,7 +42,20 @@ def read_in_pattern():
                 arr[-1].append(int(num))
 
     I = arr[0]
-#     print (I)
+
+    idx = 0
+    str_out = ''
+
+    for i in range(NUM_NEURONS):
+        str_out += str(I[i])
+        idx += 1
+
+        if idx == LINE_AMOUNT:
+            print (str_out)
+            idx = 0
+            str_out=''
+
+    print(str_out)
 
 def read_patterns():
     global patterns
@@ -51,7 +65,7 @@ def read_patterns():
 
     idx = 0
 
-    patterns_file = open("patterns.in", "r")
+    patterns_file = open("patterns_2.in", "r")
 
     for line in patterns_file:
 #         print (line)
@@ -78,11 +92,6 @@ def read_patterns():
 
 
 
-def func_to_calc_sigmoid(u):
-#     print("exp func : " +  str(u))
-#     print(2 / (1 + math.exp(-u / step_length)) - 1)
-    return 2 / (1 + math.exp(-u / step_length)) - 1
-
 
 def norm(neuron):
     sum_w = 0
@@ -99,13 +108,17 @@ def learning():
 
     for i in range(NUM_NEURONS):
         for j in range(NUM_NEURONS):
-            for pattern_num in range(NUM_PATTERNS):
-                W[i][j] = W[i][j] + patterns[pattern_num][i] * patterns[pattern_num][j]
+            if i != j:
+                for pattern_num in range(NUM_PATTERNS):
+                    W[i][j] += patterns[pattern_num][i] * patterns[pattern_num][j]
+#                     print(W)
+#                     print('i='+str(i) + ' j=' + str(j) + ' W=' + str(W[i][j]) + ' pattern:'+str(patterns[pattern_num][i]) + '/' + str(patterns[pattern_num][j]))
 
-    for i in range(NUM_NEURONS):
-        norm_w = norm(i)
-        for j in range(NUM_NEURONS):
-            W[i][j] = W[i][j] / norm_w
+
+#     for i in range(NUM_NEURONS):
+# #         norm_w = norm(i)
+#         for j in range(NUM_NEURONS):
+#             W[i][j] = W[i][j] / NUM_NEURONS #norm_w
 
 
 def calc_Dxy(step):
@@ -122,36 +135,135 @@ def calc_S(step, neuron):
 #     print("exp func : " +  str(intern_state[step][j][2]))
 
     for j in range(NUM_NEURONS):
-        if neuron != j:
-            sum_S = sum_S + W[neuron][j] * func_to_calc_sigmoid(intern_state[step][j][2])
+            sum_S += W[neuron][j] * rkf.func_to_calc_sigmoid(intern_state[step][j][2])
 
-    print(sum_S)
+#     print(sum_S)
     return sum_S
 
 
+def get_out(out_state):
+    arr = []
 
+    for i in range(NUM_NEURONS):
+        arr.append(int(round(rkf.func_to_calc_sigmoid(out_state[i][2]))))
+
+    return arr
+
+def print_out(out_state):
+
+    st_outr = ''
+    idx = 0
+
+    for i in range(NUM_NEURONS):
+        st_outr += str(int(round(rkf.func_to_calc_sigmoid(out_state[i][2]))))
+        idx += 1
+
+        if idx == LINE_AMOUNT:
+            print(st_outr)
+            st_outr=''
+            idx = 0
+
+    print(st_outr)
+
+
+def show_picture(Out_pattern, fig, num_row, num_col, plt_idx):
+
+    ax1 = fig.add_subplot(num_row,num_col,plt_idx, aspect='equal')
+    plt.axis('off')
+
+    for i in range(NUM_NEURONS):
+        if Out_pattern[i] == 1:
+            ax1.add_patch(patches.Rectangle((LINE_AMOUNT*10 - (i % LINE_AMOUNT)*10,(LINE_AMOUNT*10 - (i / LINE_AMOUNT)*10)), 10, 10))
+
+
+
+    ax1.add_patch(patches.Rectangle((10-1,10-1),LINE_AMOUNT*10+1,LINE_AMOUNT*10+1, fill=False))
+
+    plt.axis([10-1.1,LINE_AMOUNT*10+10+1.1,10-1.1,LINE_AMOUNT*10+10+1.1])
+
+    #fig1.savefig('rect1.png', dpi=90, bbox_inches='tight')
+
+
+
+
+
+
+'''
+#===============================================================================
+#                 MAIN FUNCTION
+#===============================================================================
+'''
 def run():
     global intern_state
 #     print(W)
 
 #     print(intern_state[0][0])
-
-#     np.resize(patterns, (NUM_PATTERNS,NUM_NEURONS))
-#     np.resize(I, (NUM_NEURONS))
-#     np.resize(intern_state, (NUM_STEPS, NUM_NEURONS, 4))
-#     np.resize(W, (NUM_NEURONS,NUM_NEURONS))
-
     read_patterns()
+    print(W)
     learning()
+    print(W)
     read_in_pattern()
+
+#     show_picture(I)
+    fig1 = plt.figure()
+    idx = 1
+    show_picture(I, fig1, 5, 10, idx)
+    idx += 1
+
+    print(W)
+    for i in range(len(W[0])):
+        print(W[i])
+
+#     show_picture(patterns[0], fig1, 1, 4, 1)
+#     show_picture(patterns[1], fig1, 1, 4, 2)
+#     show_picture(patterns[2], fig1, 1, 4, 3)
+#     show_picture(patterns[3], fig1, 1, 4, 4)
+#     plt.show()
+
+    for i in range(NUM_NEURONS):
+        intern_state[0][i][2] = I[i]
+
+
+#     print(W)
 
     for sstep in range(NUM_STEPS-1):
         for i in range(NUM_NEURONS):
-            print(intern_state[sstep])
-            intern_state[sstep+1][i] = rkf.solve_one_step(intern_state[sstep][i], calc_S(sstep, i), I[i], calc_Dxy(sstep))
+            #print(intern_state[sstep])
+            intern_state[sstep+1][i] = rkf.solve_one_step(intern_state[sstep][i], calc_S(sstep, i), I[i], calc_Dxy(sstep), step_length)
+
+
+        if sstep % 100 == 0:
+            print("State on step=" + str(sstep))
+            print_out(intern_state[sstep+1])
+
+            if sstep > (NUM_STEPS - 4900):
+                show_picture(get_out(intern_state[sstep+1]), fig1, 5, 10, idx)
+                idx += 1
 
 
     print("Done")
+
+
+    show_picture(get_out(intern_state[-2]), fig1, 5, 10, idx)
+
+#     for i in range(6):
+#         show_picture(get_out(intern_state[-2]), fig1, 3, 2, i)
+
+    plt.show()
+
+    arr_val = []
+    arr_time = []
+    for i in range(NUM_STEPS):
+        arr_time.append(intern_state[i][0][0])
+        arr_val.append(intern_state[i][0][3])
+
+    print(arr_time)
+    print(arr_val)
+
+    plt.plot(arr_time, arr_val)
+    plt.show()
+
+    print_out(intern_state[-2])
 
 
 run()
